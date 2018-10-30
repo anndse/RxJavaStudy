8094095222c3630496af384a72ac1858410f158b
wanghui <523693730@qq.com>
2018-10-30 11:21:58 +0800

    Observable 的 from 操作符

--- src/main/java/top/plusy/ch03/StapleOperator.java
+++ src/main/java/top/plusy/ch03/StapleOperator.java
@@ -7,7 +7,10 @@ import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class StapleOperator {
     public void createOperator(){
@@ -59,6 +62,84 @@ public class StapleOperator {
                     @Override
                     public void run() throws Exception {
                         System.out.println("Sequence complete");
+                    }
+                });
+    }
+
+    //from array
+    public void fromOperatorV1(){
+        Observable.fromArray("Hello", "World")
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        System.out.println(s);
+                    }
+                });
+    }
+
+    //from iterator
+    public void fromOperatorV2() {
+        List<Integer> items = new ArrayList<>();
+        for(int i = 0; i < 10; i+=1) {
+            items.add(i);
+        }
+
+        Observable.fromIterable(items)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        System.out.println("Next: " + integer);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        System.out.println(throwable.getMessage());
+                    }
+                }, new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        System.out.println("Sequence complete");
+                    }
+                });
+    }
+
+    //from future
+    static class MyCallable implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            System.out.println("模拟一些耗时的操作...");
+            Thread.sleep(5000L);
+            return "OK";
+        }
+    }
+
+    public void fromOperatorV3() {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<String> future = executorService.submit(new MyCallable());
+
+        Observable.fromFuture(future)
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        System.out.println(s);
+                    }
+                });
+    }
+
+    public void fromOperatorV4(){
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<String> future = executorService.submit(new MyCallable());
+
+        Observable.fromFuture(future, 4, TimeUnit.SECONDS)
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        System.out.println(s);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        throwable.printStackTrace();
                     }
                 });
     }
--- src/test/java/top/plusy/test/ch03/StapleOperatorTest.java
+++ src/test/java/top/plusy/test/ch03/StapleOperatorTest.java
@@ -6,7 +6,20 @@ public class StapleOperatorTest {
     public static void main(String[] args){
         StapleOperator stapleOperator = new StapleOperator();
         stapleOperator.createOperator();
+
         System.out.println();
         stapleOperator.justOperator();
+
+        System.out.println();
+        stapleOperator.fromOperatorV1();
+
+        System.out.println();
+        stapleOperator.fromOperatorV2();
+
+        System.out.println();
+        stapleOperator.fromOperatorV3();
+
+        System.out.println();
+        stapleOperator.fromOperatorV4();
     }
 }
